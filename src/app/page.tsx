4d081f2eaa8f8df3f74cb207a26adc57bad8f5a2
kanakanho <kahoshibakichi@gmail.com>
2024-07-02 18:44:51 +0900

    add: 自動で回転させる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,29 @@ import { ARButton } from "@react-three/xr";
 import { Canvas } from "@react-three/fiber";
 import { EffectComposer, SSAO } from "@react-three/postprocessing";
 import { Box, PerspectiveCamera } from "@react-three/drei";
-import { Vector3 } from "three";
+import { Euler, Vector3 } from "three";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const cameraPosition = new Vector3(0, 0, 0);
+  const [rotation, setRotation] = useState(new Euler(0, 0, 0));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRotation(
+        (prevRotation) =>
+          new Euler(0, prevRotation.y + Math.abs(Math.random() / 2), prevRotation.z + Math.abs(Math.random() / 1000))
+      );
+    }, 1000 / 60);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <ARButton />
       <Canvas style={{ width: "100vw", height: "100vh" }}>
         <PerspectiveCamera fov={90} makeDefault position={cameraPosition} near={1} far={200} />
-        <Box args={[1, 1, 1]} position={[0, 0, -5]} />
+        <Box args={[2, 2, 2]} position={[0, 0, -5]} rotation={rotation} />
         <EffectComposer>
           <SSAO />
         </EffectComposer>
